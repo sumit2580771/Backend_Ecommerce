d698427f13ef1c5ea70764ea8ee04eb855bc00ce
sumit2580771 <hardboy909w@gmail.com>
2025-08-26 22:28:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,29 @@ cloudinary.config({
 });
 
 const app = express();
+
+
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+//     credentials: true
+// }));
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  process.env.FRONTEND_URL, // e.g. https://your-production.com
+].filter(Boolean); // removes undefined if env is not set
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.get("/", (req, res) => {
